83069b76dcac56050e7061197c5e856ace11a0cc
Nikoloz Tevdoradze <nikoloztevdoradze08@gmail.com>
2025-08-03 02:01:30 +0400

    Install react-router-dom library, create router configuration

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import MainLayout from './layouts/MainLayout'
+
+const router = createBrowserRouter([{
+  element: <MainLayout />,
+  path: '/',
+}])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <RouterProvider router={router} />
   </StrictMode>,
 )
