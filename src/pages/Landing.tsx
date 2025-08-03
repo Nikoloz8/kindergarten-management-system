caab4221589dd0954223d0e6a39a50ce31089c6b
Nikoloz Tevdoradze <nikoloztevdoradze08@gmail.com>
2025-08-04 01:53:37 +0400

    Create statistics section in landing

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,3 +1,5 @@
+import Stats from "../components/LandingComponents/Stats"
+
 export default function Landing() {
 
     const TeacherNavArr = [
@@ -62,16 +64,22 @@ export default function Landing() {
                 </header>
             </div>
             <span className="w-full h-[1px] bg-[#E2E8F0] block mt-[14px]"></span>
-            <section className="relative w-[1290px] mt-[24px] rounded-[8px] p-[18px_0] flex justify-center items-center bg-[url('/assets/Container.svg')] bg-cover overflow-hidden bg-no-repeat bg-center">
+            <section className="relative w-[1290px] mt-[24px] rounded-[16px] p-[18px_0] flex justify-center items-center bg-[url('/assets/Container.svg')] bg-cover overflow-hidden bg-no-repeat bg-center">
                 <div className="absolute w-full h-full bg-linear-to-r from-[#0f172acc] to-[#f1f5f999] left-0"></div>
                 <div className="flex flex-col w-[670px] items-center z-10 justify-center gap-[16px]">
                     <h1 className="text-[3.6rem] font-[700] text-center text-[#FFFFFF] leading-[40px]">მოგესალმებით სკოლამდელ პლატფორმაზე!</h1>
-                    <h3 className="text-[2rem] text-center font-[400] text-[#FFFFFF] leading-[28px]">ბავშვების განვითარების თვალყურისა და მშობლებთან კომუნიკაციის ერთიანი სისტემა</h3>
-                    <button className="p-[12px_32px] rounded-[8px] bg-[rgba(255,255,255,0.2)] border-[1px] border-solid flex items-center gap-[8px] duration-300 transition-all font-[500] text-[1.4rem] leading-[20px] cursor-pointer text-[#FFFFFF] border-[rgba(255,255,255,0.4)]">
+                    <h3 className="text-[2rem] text-center font-[400] text-[#FFFFFF] leading-[28px]">ბავშვების განვითარების მეთვალყურეობისა და მშობლებთან კომუნიკაციის ერთიანი სისტემა</h3>
+                    <button className="p-[12px_32px] rounded-[8px] bg-[rgba(255,255,255,0.2)] border-[1px] border-solid flex items-center gap-[8px] duration-300 transition-all font-[500] text-[1.4rem] leading-[20px] cursor-pointer hover:scale-110 text-[#FFFFFF] border-[rgba(255,255,255,0.4)]">
                         <img src="/assets/SVG9.svg" alt="" />
                         დღევანდელი მონაცემები
                     </button>
                 </div>
+            </section>
+            <section className="w-[1290px] mt-[32px] flex gap-[24px]">
+                <Stats title="ბავშვები კლასში" count="18" stat="15 დამსწრე დღეს"/>
+                <Stats title="ბავშვები კლასში" count="18" stat="15 დამსწრე დღეს"/>
+                <Stats title="ბავშვები კლასში" count="18" stat="15 დამსწრე დღეს"/>
+                <Stats title="ბავშვები კლასში" count="18" stat="15 დამსწრე დღეს"/>
             </section>
         </div>
     )
